b5b8283e5563f69e4c6c9b0ed9674bb3b1089d08
MarinaNakvas <marfuny51@gmail.com>
2020-05-30 00:50:25 +0300

    scroll

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+/*document.onwheel = function (event) {
+    console.log(event);
+    if (1450 < event.deltaY > 1865 ) {
+       document.getElementById('list').scrollLeft += 15;
+    }
+}*/
+
+
+
+$('#list').bind('mousewheel', function(event, delta) {
+    if(delta ==1) {
+        this.scrollLeft -= 200;
+    }
+    else {
+        this.scrollLeft += 200;
+    }
+    event.preventDefault();
+});
 
