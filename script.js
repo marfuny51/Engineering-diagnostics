31786de9494f6f1a9284e9fc0b337b7ee66bec26
MarinaNakvas <marfuny51@gmail.com>
2020-05-30 10:47:20 +0300

    adaptive

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
-/*document.onwheel = function (event) {
+let elem = document.getElementById('list');
+
+elem.onwheel = function (event) {
     console.log(event);
     if (1450 < event.deltaY > 1865 ) {
-       document.getElementById('list').scrollLeft += 15;
-    }
-}*/
-
-
-
-$('#list').bind('mousewheel', function(event, delta) {
-    if(delta ==1) {
-        this.scrollLeft -= 200;
+        elem.scrollLeft -= 200;
     }
     else {
-        this.scrollLeft += 200;
+        elem.scrollLeft += 200;
     }
     event.preventDefault();
-});
+}
+
 
