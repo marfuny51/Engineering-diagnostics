bf6b685b5051fdedd8f4240d609fc0da19305598
MarinaNakvas <marfuny51@gmail.com>
2020-06-01 16:25:09 +0200

    save

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ let elem = document.getElementById('list');
 elem.onwheel = function (event) {
     console.log(event);
     if (1450 < event.deltaY > 1865 ) {
-        elem.scrollLeft -= 200;
+        elem.scrollLeft -= 100;
     }
     else {
-        elem.scrollLeft += 200;
+        elem.scrollLeft += 100;
     }
     event.preventDefault();
 }
